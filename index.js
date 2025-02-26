1cc793a37bc40ce2bc3b6eee4f4aace4f66fe4fe
H4UZ1CHK1 <navalnyalexey4@gmail.com>
2025-02-26 13:31:47 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-console.log(`Имя: ${process.env.FIRST_NAME}`);
-console.log(`Фамилия: ${process.env.LAST_NAME}`);
-console.log(`Номер группы: ${process.env.GROUP_NUMBER}`);
-console.log(`Номер по списку: ${process.env.LIST_NUMBER}`);
